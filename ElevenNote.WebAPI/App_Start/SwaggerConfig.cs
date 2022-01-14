54afe24374950201220e7be3ea3abd0d7ebb457a
Ellie Hong <hongek9@gmail.com>
2022-01-13 19:30:56 -0500

    extra things

--- ElevenNote.WebAPI/App_Start/SwaggerConfig.cs
+++ ElevenNote.WebAPI/App_Start/SwaggerConfig.cs
@@ -2,11 +2,82 @@ using System.Web.Http;
 using WebActivatorEx;
 using ElevenNote.WebAPI;
 using Swashbuckle.Application;
+using System.Linq;
+using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
 namespace ElevenNote.WebAPI
 {
+
+    public class AddAuthorizationHeaderParameterOperationFilter: IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            var filterPipeline = apiDescription.ActionDescriptor.GetFilterPipeline();
+            var isAuthorized = filterPipeline.Select(filterInfo => filterInfo.Instance).Any(filter => filter is IAuthenticationFilter);
+
+            var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+
+            if (!isAuthorized || allowAnonymous) return;
+
+            if (operation.parameters == null) operation.parameters = new List<Parameter>();
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "from /token endpoint",
+                required = true,
+                type = "string,"
+            });
+        }
+    }
+
+     class AuthTokenEndpointOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
     public class SwaggerConfig
     {
         public static void Register()
@@ -33,6 +104,8 @@ namespace ElevenNote.WebAPI
                         // additional fields by chaining methods off SingleApiVersion.
                         //
                         c.SingleApiVersion("v1", "ElevenNote.WebAPI");
+                        c.OperationFilter(() => new AddAuthorizationHeaderParameterOperationFilter());
+                        c.DocumentFilter<AuthTokenEndpointOperation>();
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
@@ -170,7 +243,7 @@ namespace ElevenNote.WebAPI
                         // with the same path (sans query string) and HTTP method. You can workaround this by providing a
                         // custom strategy to pick a winner or merge the descriptions for the purposes of the Swagger docs
                         //
-                        //c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                        c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 
                         // Wrap the default SwaggerGenerator with additional behavior (e.g. caching) or provide an
                         // alternative implementation for ISwaggerProvider with the CustomProvider option.
